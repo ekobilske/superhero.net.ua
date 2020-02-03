c867ac6fa2edf9450d1153da290ccf80b3afb5e0
Eugene Kobeliaksky <ekobilske@gmail.com>
2020-02-03 21:46:54 +0200

    create header component

--- web-site_khudienko/src/components/layouts/navbar.vue
+++ web-site_khudienko/src/components/layouts/navbar.vue
@@ -1,13 +1,18 @@
 <template>
     <section class="header">
-        <section class="header__wrap-section">
+        <section class="header__wrap-section content-position">
             <div class="header__wrap-section__logo">
-                <a href="">
-                    <p>GOGOGOGOGO</p>
+                <a href="superhero.net.ua"> 
                     <img src="src/assets/images/logo.png" alt="logo superhero">
                 </a>
             </div>
-            <div class="header__wrap-section__navbar"></div>
+            <ul class="header__wrap-section__navbar">
+                <li>про нас</li>
+                <li>ціни</li>
+                <li>акції</li>
+                <li>галерея</li>
+                <li>контакти</li>
+            </ul>
         </section>
     </section>
 </template>
