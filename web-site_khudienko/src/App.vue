6f50360f1f400b6f680159b9eef9286d0f1f511a
Eugene Kobeliaksky <ekobilske@gmail.com>
2020-02-03 15:37:23 +0200

    other edit

--- web-site_khudienko/src/App.vue
+++ web-site_khudienko/src/App.vue
@@ -22,11 +22,6 @@
 <script>
 export default {
   name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
-  }
 }
 </script>
 
---
+++ web-site_khudienko/src/components/layouts/navbar.vue
@@ -0,0 +1,21 @@
+<template>
+    <section class="header">
+        <section class="header__wrap-section">
+            <div class="header__wrap-section__logo">
+                <a href="">
+                    <p>GOGOGOGOGO</p>
+                    <img src="src/assets/images/logo.png" alt="logo superhero">
+                </a>
+            </div>
+            <div class="header__wrap-section__navbar"></div>
+        </section>
+    </section>
+</template>
+<script>
+    export default {
+        name: 'navbar'
+    }
+</script>
+<style lang="scss" scoped>
+    @import "src/scss/navbar.scss"
+</style>
