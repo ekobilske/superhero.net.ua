8be9ad1226ca793c67227c27101cb83ce33b4dca
Eugene Kobeliaksky <ekobilske@gmail.com>
2020-02-04 00:27:27 +0200

    i have problem for bg img

--- web-site_khudienko/src/components/modules/announcement.vue
+++ web-site_khudienko/src/components/modules/announcement.vue
@@ -1,6 +1,28 @@
 <template>
     <section class="wrap-info">
-        
+        <div class="info-content content-position">
+            <h1 class="title-info">АнонС</h1>
+            <section class="info-wrap">
+                <div class="info-column">
+                    <img class="info-icon">
+                    <div class="info-descript">
+
+                    </div>
+                </div>
+                <div class="info-column">
+                    <img class="info-icon">
+                    <div class="info-descript">
+
+                    </div>
+                </div>
+                <div class="info-column">
+                    <img class="info-icon">
+                    <div class="info-descript">
+
+                    </div>
+                </div>
+            </section>
+        </div>
     </section>
 </template>
 <script>
