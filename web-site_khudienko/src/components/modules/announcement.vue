d9bf18afe9564707e9ec12db778f3fe89707f913
Eugene Kobeliaksky <ekobilske@gmail.com>
2020-02-04 23:59:52 +0200

    create info component

--- web-site_khudienko/src/components/modules/announcement.vue
+++ web-site_khudienko/src/components/modules/announcement.vue
@@ -4,21 +4,26 @@
             <h1 class="title-info">АнонС</h1>
             <section class="info-wrap">
                 <div class="info-column">
-                    <img class="info-icon">
+                    <img class="info-icon" src="src/assets/images/icon/date.svg">
                     <div class="info-descript">
-
+                        <p class="info-date">14.02</p>
+                        <div class="info-underline"></div>
+                        <p class="info-date">15.03</p>
                     </div>
                 </div>
                 <div class="info-column">
-                    <img class="info-icon">
+                    <img class="info-icon" src="src/assets/images/icon/time.svg">
                     <div class="info-descript">
-
+                        <p class="info-time">працюємо:</p>
+                        <p class="info-time time-num">10<sup>00</sup> - 20<sup>00</sup></p>
                     </div>
                 </div>
                 <div class="info-column">
-                    <img class="info-icon">
+                    <img class="info-icon" src="src/assets/images/icon/location.svg">
                     <div class="info-descript">
-
+                        <p class="location-city">Вінниця</p>
+                        <img class="location-logo" src="src/assets/images/location-logo.png">
+                        <p class="location-street">вулиця Келецька, 117-Б</p>
                     </div>
                 </div>
             </section>
