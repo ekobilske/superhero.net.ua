7c488b9073d8fd2bb1eb6cb6cb9a8a6df351ccfe
Eugene Kobeliaksky <ekobilske@gmail.com>
2020-02-05 23:40:50 +0200

    add font and connected

--- web-site_khudienko/src/components/modules/content-block.vue
+++ web-site_khudienko/src/components/modules/content-block.vue
@@ -2,17 +2,42 @@
     <section class="content-block">
         <section class="content-wrap content-position">
             <img class="title-content" src="src/assets/images/title-content.png">
-            <article class="content-text-wrap">
-                <p>Вже у Вашому місті єдина виставка в Україні ”Всесвіт СУПЕРГЕРОЇВ та Minecraft”.
+            <div class="separate-wrap">
+                <div class="wrap-line">
+                    <div class="sup-line"></div>
+                    <div class="sub-line"></div>
+                </div>
+                <img class="separate-icon" src="src/assets/images/hero-icon.png">
+                <div class="wrap-line">
+                    <div class="sup-line"></div>
+                    <div class="sub-line"></div>
+                </div>
+            </div>
+            <!--description text-->
+            <!--description text-->
+            <section class="content-text-wrap">
+                <p class="content-text">Вже у Вашому місті єдина виставка в Україні ”Всесвіт СУПЕРГЕРОЇВ та Minecraft”.
                 Під час відвідування ви зможете зануритися в таємничий віртуальний світ фантастики,
                 зробити незабутні фото з героями та їх атрибутами, прокататися на бетмобілі,
                 позмагатися з героями “Minecraft”, приміряти костюми героїв, спробувати подужати молот Тора...
                 і багато іншого. Ми чекаємо саме на тебе. Не упусти шанс подарувати собі та рідним незабутні враження.
                 Міста чекають на нас. Приєднуйся і Ти.</p>
-                <p>P.S. Діє акційна система відвідування.</p>
-				<p>Дітям - безкоштовно.</p>
-                
-            </article>
+                <p class="content-ps1">P.S. Діє акційна система відвідування.</p>
+				<p class="content-ps2">Дітям - безкоштовно.</p>    
+            </section>
+            <!--/description text-->
+            <!--/description text-->
+            <div class="separate-wrap">
+                <div class="wrap-line">
+                    <div class="sup-line"></div>
+                    <div class="sub-line"></div>
+                </div>
+                <img class="separate-icon" src="src/assets/images/hero-icon.png">
+                <div class="wrap-line">
+                    <div class="sup-line"></div>
+                    <div class="sub-line"></div>
+                </div>
+            </div>
         </section>
     </section>
 </template>
