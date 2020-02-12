b6ee2df8189b74dafdb35d73e34a748a03afbe2f
Eugene Kobeliaksky <ekobilske@gmail.com>
2020-02-12 21:20:53 +0200

    add mobile sidebar

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $("#btn-down").click(function(event){
+        $("#mobile-bar").css("display", "block");
+    });
+    $("#btn-up").click(function(event){
+        $("#mobile-bar").css("display", "none");
+    });
+})
